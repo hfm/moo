982df9caa41904495014f52ff64b7bda808956b3
OKUMURA Takahiro <hfm.garden@gmail.com>
2015-10-04 17:42:14 +0900

    Initialize

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject moo "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject moo "1.0.0"
+  :description "A game like Bulls and Cows, Hit and Blow, or Breaker."
+  :url "https://github.com/tacahilo/moo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :main moo.core)
--- test/moo/core_test.clj
+++ test/moo/core_test.clj
@@ -1,7 +1,3 @@
 (ns moo.core-test
-  (:require [clojure.test :refer :all]
-            [moo.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (:use
+    moo.core))
