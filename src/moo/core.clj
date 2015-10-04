4d5d14d28604f6a30d5fdda2291135b2d3fbbc52
OKUMURA Takahiro <hfm.garden@gmail.com>
2015-10-04 18:24:45 +0900

    Main (not implemented)

--- src/moo/core.clj
+++ src/moo/core.clj
@@ -1,3 +1,10 @@
 (ns moo.core
   (:use
     [midje.sweet :only (unfinished)]))
+
+(unfinished init-model init-view run-shell)
+(defn -main
+  [& args]
+  (init-model)
+  (init-view)
+  (run-shell))
--- test/moo/core_test.clj
+++ test/moo/core_test.clj
@@ -2,3 +2,10 @@
   (:use
     moo.core
     midje.sweet))
+
+(fact "about -main"
+      (-main) => ..C..
+      (provided
+        (init-model) => ..A.. :times 1
+        (init-view) => ..B.. :times 1
+        (run-shell) => ..C.. :times 1))
